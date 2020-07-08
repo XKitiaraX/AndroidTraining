74a31e7e277560d1186d868a91862a89743e2d54
Paula Arena <paularodsor@gmail.com>
2020-07-08 10:40:09 +0200

    TrackMySleepQuality Update 1
    Codelabs - Android Kotlin Fundamentals 06.2: Coroutines and Room

--- Codelabs/Android Kotlin Fundamentals Course/TrackMySleepQuality-Starter/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ Codelabs/Android Kotlin Fundamentals Course/TrackMySleepQuality-Starter/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -28,16 +28,25 @@ abstract class SleepDatabase : RoomDatabase() {
     companion object {
         @Volatile
         private var INSTANCE: SleepDatabase? = null
-    }
 
-    fun getInstance(context: Context): SleepDatabase {
-        synchronized(this) {
-            var instance = INSTANCE
-            if (instance == null) {
-                instance = Room.databaseBuilder(context.applicationContext, SleepDatabase::class.java, "sleep_history_database").fallbackToDestructiveMigration().build()
-                INSTANCE = instance
+        fun getInstance(context: Context): SleepDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                            context.applicationContext,
+                            SleepDatabase::class.java,
+                            "sleep_history_database"
+                    )
+                            .fallbackToDestructiveMigration()
+                            .build()
+                    INSTANCE = instance
+                }
+                return instance
             }
-            return instance
         }
     }
+
+
 }
